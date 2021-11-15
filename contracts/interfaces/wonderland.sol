bc9121f880c863a2cfe4eae5759c3edca5e347ed
Abigail <abigailtcameron@gmail.com>
2021-11-15 16:23:39 -0500

    breakpoint

--- contracts/interfaces/wonderland.sol
+++ contracts/interfaces/wonderland.sol
@@ -58,3 +58,27 @@ interface ITreasury {
     function valueOf( address _token, uint _amount ) external view returns ( uint value_ );
     function mintRewards( address _recipient, uint _amount ) external;
 }
+
+interface ITimeBondDepository {
+    function initializeBondTerms( 
+        uint _controlVariable, 
+        uint _minimumPrice,
+        uint _maxPayout,
+        uint _maxDebt,
+        uint _initialDebt,
+        uint32 _vestingTerm
+    ) external;
+
+    enum PARAMETER { VESTING, PAYOUT, DEBT, MINPRICE }
+
+    function setBondTerms ( PARAMETER _parameter, uint _input ) external; 
+
+    function setAdjustment ( bool _addition, uint _increment, uint _target, uint32 _buffer ) external; 
+
+    function setStaking( address _staking, bool _helper ) external; 
+    
+    function deposit( uint _amount, uint _maxPrice, address _depositor) external payable returns ( uint );
+
+    function redeem( address _recipient, bool _stake ) external returns ( uint ) 
+
+}
