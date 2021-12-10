d8c75f6c4a41b0c7210e992558189404f621e5ed
Abigail <abigailtcameron@gmail.com>
2021-12-09 23:51:05 -0500

    wonderland base

--- contracts/strategies/strategy-wonderland-base.sol
+++ contracts/strategies/strategy-wonderland-base.sol
@@ -10,6 +10,7 @@ import "../interfaces/globe.sol";
 import "../interfaces/joe.sol";
 import "../interfaces/wonderland.sol"; 
 import "../interfaces/controller.sol";
+import "../interfaces/wavax.sol";
 
 //Wonderland Strategy Contract Basics
 abstract contract TimeBase {
@@ -20,18 +21,22 @@ abstract contract TimeBase {
 
 
     // staking contract for rewards 
-    address public staking = 0x4456B87Af11e87E329AB7d7C7A246ed1aC2168B9; 
+    address public staking = 0x4456B87Af11e87E329AB7d7C7A246ed1aC2168B9;
+    
+    uint256 public maxPrice; 
+    
 
     // Tokens
-    address public want; 
+    address public want;  
     address public constant joe = 0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd;
     address public constant wavax = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
     address public constant snob = 0xC38f41A296A4493Ff429F1238e030924A1542e50;
-    address public constant Time = 0xb54f16fB19478766A268F172C9480f8da1a7c9C3;          // token given as payment for bond
-    address public constant Memories = 0x136Acd46C134E8269052c62A67042D6bDeDde3C9;
+    address public constant time = 0xb54f16fB19478766A268F172C9480f8da1a7c9C3;          // token given as payment for bond
+    address public constant memo = 0x136Acd46C134E8269052c62A67042D6bDeDde3C9;
 
-    address public stakingHelper;                                                       // to stake and claim if no staking warmup
+    address public stakingHelper = 0x096BBfB78311227b805c968b070a81D358c13379;          // to stake and claim if no staking warmup
     bool public useHelper;
+    bool _stake; 
 
     // Dex
     address public constant joeRouter = 0x60aE616a2155Ee3d9A68541Ba4544862310933d4;
@@ -117,7 +122,7 @@ abstract contract TimeBase {
         return balanceOfWant().add(balanceOfPool());
     }
 
-     function balanceOfTime() public virtual view returns (uint256);
+    function balanceOfTime() public virtual view returns (uint256);
 
     function getName() external virtual pure returns (string memory);
   
@@ -183,7 +188,7 @@ abstract contract TimeBase {
     // **** State mutations **** //
     function deposit() public virtual;
 
-    function depositIntoTime() public virtual;
+    function depositBond() public virtual;
 
     // Controller only function for creating additional rewards from dust
     function withdraw(IERC20 _asset) external returns (uint256 balance) {
@@ -223,7 +228,9 @@ abstract contract TimeBase {
 
     function harvest() public virtual;
 
-    function reStake() public virtual;
+    function _withdrawAll() internal {
+        _withdrawSome(balanceOfPool());
+    }
 
     function _withdrawSome(uint256 _amount) internal virtual returns (uint256);
 
@@ -278,12 +285,16 @@ abstract contract TimeBase {
     }
 
     function _takeFeeTimeToSnob(uint256 _keep) internal {
+        IERC20(memo).safeApprove(staking, 0);
+        IERC20(memo).safeApprove(staking, _keep);
+        ITimeStaking(staking).unstake(_keep, true); 
+
         address[] memory path = new address[](3);
-        path[0] = Time;
+        path[0] = time;
         path[1] = wavax;
         path[2] = snob;
-        IERC20(Time).safeApprove(joeRouter, 0);
-        IERC20(Time).safeApprove(joeRouter, _keep);
+        IERC20(time).safeApprove(joeRouter, 0);
+        IERC20(time).safeApprove(joeRouter, _keep);
         _swapTraderJoeWithPath(path, _keep);
         uint256 _snob = IERC20(snob).balanceOf(address(this));
         uint256 _share = _snob.mul(revenueShare).div(revenueShareMax);
@@ -292,21 +303,6 @@ abstract contract TimeBase {
             IController(controller).treasury(),
             _snob.sub(_share)
         );
-    }
-
-    function stakeOrSend( address _recipient, bool _stake, uint _amount ) internal returns ( uint ) {
-        if ( !_stake ) { // if user does not want to stake
-            IERC20(Time).transfer( _recipient, _amount ); // send payout
-        } else { // if user wants to stake
-            if ( useHelper ) { // use if staking warmup is 0
-                IERC20(Time).approve( address(stakingHelper), _amount );
-                IStakingHelper(stakingHelper).stake( _amount, _recipient );
-            } else {
-                IERC20(Time).approve( address(staking), _amount );
-                IStaking(staking).stake( _amount, _recipient );
-            }
-        }
-        return _amount;
     }
 
     function _distributePerformanceFeesAndDeposit() internal {
